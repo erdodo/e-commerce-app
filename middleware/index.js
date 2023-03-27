e9402ca705d80de1503fd09e972101d63733c996
erdodo <erdoganyesil3@gmail.com>
2023-03-27 05:07:12 +0300

    Compose update

--- middleware/index.js
+++ middleware/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 module.exports =(req,res,next)=> {
     console.log("Middleware is working 1111")
     mongoose.connect("mongodb://localhost:27017", {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
-        res.send("MongoDB connected 1111")
+        res.send("MongoDB connected 1112221")
     })
     /*if(req.header('Authorization') != undefined){
         jwt.verify(req.header('Authorization').split(' ')[1], process.env.JWT_SECRET_KEY, (err, authData) => {
