f44049c13063512c510c0a9192cf6b8e3a778355
erdodo <erdoganyesil3@gmail.com>
2023-03-27 03:25:20 +0300

    Compose update

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ app.use((req, res, next) => {
 
 app.use('/', routes)
 
-mongoose.connect("mongodb://localhost:27017/ecommerce", {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+mongoose.connect("mongodb://localhost:27017", {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     console.log("MongoDB connected")
 })
 
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port aaa ${port}`)
 })
--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,12 @@
 const jwt = require("jsonwebtoken");
 const Customer = require("../models/Customer");
+const mongoose = require("mongoose");
 module.exports =(req,res,next)=> {
-    console.log("Middleware is working")
-    if(req.header('Authorization') != undefined){
+    console.log("Middleware is working 1111")
+    mongoose.connect("mongodb://localhost:27017", {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+        res.send("MongoDB connected")
+    })
+    /*if(req.header('Authorization') != undefined){
         jwt.verify(req.header('Authorization').split(' ')[1], process.env.JWT_SECRET_KEY, (err, authData) => {
             if (err) {
                 res.json({message: "Token is invalid",token:req.header('Authorization')} )
@@ -15,6 +19,6 @@ module.exports =(req,res,next)=> {
         })
     }else{
         res.json({message: "Token is invalid",token:req.header('Authorization')} )
-    }
+    }*/
 
 }
