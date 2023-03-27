5c11314c5777785ec6f8ce31240fc924f041bf38
erdodo <erdoganyesil3@gmail.com>
2023-03-27 03:17:13 +0300

    Compose update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 
 app.use('/', routes)
 
-mongoose.connect("mongodb://18.192.107.245:32769/ecommerce", {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+mongoose.connect("mongodb://18.192.107.245:27017/ecommerce", {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
     console.log("MongoDB connected")
 })
 
